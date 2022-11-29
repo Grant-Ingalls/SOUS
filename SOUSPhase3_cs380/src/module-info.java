19a8371acfb70279cd0075a299e546a8b27e8b70
Grant-Ingalls <114887076+Grant-Ingalls@users.noreply.github.com>
2022-11-29 01:44:02 -0800

    Delete SOUSPhase3_cs380 directory

--- SOUSPhase3_cs380/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module Lab3_cs380 {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.base;
-	requires javafx.graphics;
-	requires java.sql;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
